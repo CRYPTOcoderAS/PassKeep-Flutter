29a9670c15c1cba096dfc02fac4fa41ef62805e6
CryptocoderAS <as7663@srmist.edu.in>
2021-11-14 17:47:18 +0530

    :gift: AppDrawer

--- lib/viewer.dart
+++ lib/viewer.dart
@@ -4,6 +4,7 @@ import 'package:passkeep/gloab.dart';
 import 'package:passkeep/lowlevel/file.dart';
 import 'package:passkeep/passViewer.dart';
 import 'package:passkeep/widgets.dart';
+import 'package:passkeep/entry.dart';
 
 class ViewerInit extends StatelessWidget {
   @override
@@ -26,6 +27,74 @@ class Viewer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("images/icon.png"),
+                backgroundColor: Colors.white,
+                //backgroundImage: Image(image: AssetImage('images/logo.jpg', width: 100.0, height: 100.0)),
+              ),
+              accountEmail: Text(
+                "https://sachan1.netlify.app/",
+                style: TextStyle(color: Colors.blue),
+              ),
+              accountName: Text('Akshat Sachan',
+                  style: TextStyle(fontSize: 24.0, color: Colors.teal)),
+              decoration: BoxDecoration(
+                color: Colors.black87,
+              ),
+            ),
+            ListTile(
+              // leading: const Icon(Icons.house),
+              title: const Text(
+                'THANKS FOR USING !!',
+                style: TextStyle(fontSize: 24.0, color: Colors.blue),
+              ),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => Entry(
+                        //text: 'Houses',
+                        ),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              // leading: const Icon(Icons.apartment),
+              title: const Text(
+                '\n\nMADE BY : Akshat Sachan\n\n',
+                style: TextStyle(fontSize: 18.0),
+              ),
+
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => Entry(),
+                    //  title: 'Apartments',
+                  ),
+                  // ),
+                );
+              },
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Image.asset(
+                'images/logo.jpg',
+                width: 800.0,
+                height: 800.0,
+              ),
+              //backgroundImage: Image(image: AssetImage('images/logo.jpg', width: 100.0, height: 100.0)),
+            ),
+          ],
+        ),
+      ),
+      // appBar: _appBar(),
       backgroundColor: back,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
